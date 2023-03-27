ebc903acdf0373d8317606d6a0ad30108f062e36
Anastasiia Bilska <nastya.bilskaya@gmail.com>
2023-03-27 16:29:49 +0300

    modal fix

--- src/components/AppNavigation/AppNavigation.vue
+++ src/components/AppNavigation/AppNavigation.vue
@@ -6,20 +6,15 @@ export default {
   components: { ModalWindow },
   data() {
     return {
-      isCatalogOpen: false,
-      isCabinetOpen: false
+      isModalOpen: false,
     };
   },
   methods: {
-    openCatalogModal() {
-      this.isCatalogOpen = true;
-    },
-    openCabinetModal() {
-      this.isCabinetOpen = true;
+    openModal() {
+      this.isModalOpen = true;
     },
     closeModal() {
-      this.isCatalogOpen = false;
-      this.isCabinetOpen = false;
+      this.isModalOpen = false;
     }
   }
 };
@@ -28,22 +23,39 @@ export default {
 <template>
   <div class="container container__navigation">
     <nav class="navigation page__navigation">
-      <button type="button" @click="openCatalogModal" class="navigation__link">
+      <button type="button" @click="openModal" class="navigation__link">
         <div class="navigation__icon navigation__icon--catalog"></div>
         Каталог
       </button>
-      <button  type="button" @click="openCabinetModal" class="navigation__link">
+      <button type="button" @click="openModal" class="navigation__link">
         <div class="navigation__icon navigation__icon--me"></div>
         Kабінет
       </button>
       <div class="navigation__separator"></div>
     </nav>
-    <ModalWindow v-show="isCabinetOpen || isCatalogOpen">
-      <div>
-        <h3 class="modal__nav" v-if="isCabinetOpen">Цe кабінет!</h3>
-        <h3 class="modal__nav" v-if="isCatalogOpen">Це каталог!</h3>
-        <button @click="closeModal" class="modal__close">ок</button>
-      </div>
+    <ModalWindow v-show="isModalOpen">
+      <img class="modal__logo" src="../../assets/static/logo.svg" alt="Логотип компанії 'Хапай'" />
+      <p class="modal__text">
+        Соцмережа постачальників та покупців для покупки товарів та обміну відгуками:
+      </p>
+      <ul class="modal__list">
+        <li class="modal__list-item">1 000 000+ доступних товарів в каталозі</li>
+        <li class="modal__list-item">пошук серед товарів у наявності</li>
+        <li class="modal__list-item">перевірені постачальники товарів в Україні.</li>
+      </ul>
+      <p class="modal__text modal__text--last">
+        Залиште ваш відгук
+        <a
+          href="https://forms.gle/CKuK2yKpvPXbTLVQ9"
+          class="modal__link"
+          target="_blank"
+          title="Опитування у Google Forms"
+          aria-label="Опитування у Google Forms"
+        >
+          https://forms.gle/CKuK2yKpvPXbTLVQ9
+        </a>
+      </p>
+      <button @click="closeModal" class="modal__close">Ок</button>
     </ModalWindow>
   </div>
 </template>
